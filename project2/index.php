c512e91feb03d2478c606bac18591ab0572f5818
deebhatia <deep23bhatia@gmail.com>
2014-03-10 03:33:36 +0530

    Create index.php

---
+++ project2/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Attendance Management System</title>
+<meta name="keywords" content="tile, buttons, metro, button, ui, template, theme, modern, clean, css, css3 " />
+<meta name="description" content="SONHLAB Tile Buttons for Metro UI Template - CSS Plugins" />
+
+<script type="text/javascript">
+	if (top != self) {
+		window.open(self.location.href, '_top');
+	}
+</script>
+
+<!--<link href='http://fonts.googleapis.com/css?family=Archivo+Narrow' rel='stylesheet' type='text/css' />-->
+<link rel="stylesheet" href="css/sonhlab-tile-buttons.css" type="text/css" />
+<link rel="stylesheet" href="/project2/css/ll.css" type="text/css">
+
+</head> 
+<body>
+
+<div class="a"  >
+<center>		
+<p>Attendance Management System</p>
+</center>
+</div>
+<div>
+<center>
+	<img src="/project2/images/2.png" height="150"/>
+</center>
+</div>
+
+
+<!-- MAINFORM -->
+<div id="mainform">
+
+</br></br></br></br>	
+    <div class="button-holder">
+    
+        
+        <!-- Button 1 -->
+       
+        <div class="tile-bt-2 solid-darkgreen shadow-black floatleft margin-5" >
+        
+        	<a href="/project2/php/tchr_login_2.php" class="gradient">
+                
+                <span class="light-text">Login</span>
+            </a>
+            
+        </div>
+               
+        <!-- End Button 1 -->
+        
+        <!-- Button 2 -->
+        
+        <div class="tile-bt-2 solid-darkgreen shadow-black floatleft margin-5">
+        	<a href="/project2/php/att_check.php" class="gradient">
+                
+                <span class="light-text">Check Attendance</span>
+            </a>
+        </div>
+        
+        <!-- End Button 2 -->
+        
+        
+</div>
+<!-- END MAINFORM -->
+</body>
+</html>
